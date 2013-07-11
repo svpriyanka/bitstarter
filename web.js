6ee5d0dacc8b19efc6495dfd652f765e6abb97b0
svpriyanka <svpriyanka03@gmail.com>
2013-07-10 23:30:49 -0400

    Updated  web.js

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
 
+var fs=require('fs');
+var buffer=new Buffer(fs.readFileSync('index.html'));
+var str=buffer.toString('utf-8');
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+    response.send(str);
 });
 
 var port = process.env.PORT || 5000;
